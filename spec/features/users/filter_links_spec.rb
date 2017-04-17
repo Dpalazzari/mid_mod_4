224b8477f03c9a87b7d944294d851d246971a0f4
Drew Palazzari <drewpalazzari@hotmail.com>
2017-04-17 11:18:15 -0600

    Test filter buttons

--- spec/features/users/filter_links_spec.rb
+++ spec/features/users/filter_links_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Can filter links", type: :feature, :js => :true do
   before(:each) do
     user   = create(:user)
     link   = Link.create(url:"https://www.turing.io", title:"Turing")
-    link2  = Link.create(url:"https://www.youtube.com", title:"Youtube")
+    link2  = Link.create(url:"https://www.youtube.com", title:"Youtube", read: true)
     user.links << link
     user.links << link2
     page.set_rack_session(user_id: user.id)
@@ -22,6 +22,22 @@ RSpec.describe "Can filter links", type: :feature, :js => :true do
 
   scenario 'Filter the Turing link' do
     fill_in "filter-link", with: 'tur'
+    within('#all-links') do
+      expect(page).to_not have_content("Youtube")
+      expect(page).to have_content("Turing")
+    end
+  end
+
+  scenario 'Filtering by the links that are read' do
+    click_on "Show Read"
+    within('#all-links') do
+      expect(page).to have_content("Youtube")
+      expect(page).to_not have_content("Turing")
+    end
+  end
+
+  scenario 'Filtering by the links that are unread' do
+    click_on "Show Unread"
     within('#all-links') do
       expect(page).to_not have_content("Youtube")
       expect(page).to have_content("Turing")
